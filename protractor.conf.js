e22cf37ee4221fc0bfbb894d8884c7e878ae1717
Joshua Warejko <josh@newleaf.team>
2016-07-20 00:29:03 -0700

    Starting tests

---
+++ protractor.conf.js
@@ -0,0 +1,17 @@
+exports.config = {
+  capabilities: {
+    browserName: "chrome"
+  },
+  framework: 'mocha',
+  specs: [
+    'test/e2e/**/*.spec.js'
+  ],
+  mochaOpts: {
+    enableTimeouts: false
+  },
+  onPrepare: function () {
+    process.env.PORT = 3001
+    require('./server')
+  },
+  chromeOnly: true
+}
